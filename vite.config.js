8ecf9b46b9ae0ee936c2ab552cccdb665d378057
koji <koji.19@icloud.com>
2026-04-04 13:26:19 +0000

    queueメール送信調整

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: './', 
+
     plugins: [
         laravel({
             input: [
